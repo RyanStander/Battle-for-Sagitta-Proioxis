bab9f70799e9e9c2528a2184a967cd15b1eae330
Ryan <stander.ryan@gmail.com>
2022-01-25 21:16:26 +0100

    Full project push
    This is everything that was worked on back in 2020 September

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] int maxHealth = 10;
+    [SerializeField] int curHealth;
+    [SerializeField] int regenerateAmount = 1;
+    [SerializeField] float regenerationRate = 2f;
+
+    private void Start()
+    {
+        curHealth = maxHealth;
+
+        InvokeRepeating("Regenerate", regenerationRate, regenerationRate);
+    }
+
+    void Regenerate()
+    {
+        //Debug.Log("Regeneration: " + curHealth);
+
+        if (curHealth < maxHealth)
+            curHealth += regenerateAmount;
+
+        if (curHealth > maxHealth)
+            curHealth = maxHealth;
+
+        EventManager.TakeDamage(curHealth / (float)maxHealth);
+    }
+    public void TakeDamage(int dmg = 1)
+    {
+        curHealth -= dmg;
+        if (curHealth < 0)
+            curHealth = 0;
+
+        EventManager.TakeDamage(curHealth/(float)maxHealth);
+        if (curHealth < 1)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+
+            Explosions temp = transform.GetComponent<Explosions>();
+            EventManager.PlayerDeath();
+            temp.CollidedWithEnemy();
+            FindObjectOfType<AudioManager>().Stop("Engine");
+
+        }
+    }
+    public int GetMaxHP()
+    {
+        return maxHealth;
+    }
+}
